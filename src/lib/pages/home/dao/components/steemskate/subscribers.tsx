f1bc5bbaecf419fc08f9c50562e1362743c0167f
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-11-06 20:37:56 -0300

    last update in subs

--- src/lib/pages/home/dao/components/steemskate/subscribers.tsx
+++ src/lib/pages/home/dao/components/steemskate/subscribers.tsx
@@ -1,42 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Box, Text, Avatar, Flex, Button, Image, Input, Link, Divider,Table, Thead, Tbody, Tr, Th, Td, HStack, ModalOverlay, Modal} from '@chakra-ui/react';
+import { Box, Text, Avatar, Flex, Button, Image, Input, Link, Divider, Table, Tbody, Tr, Td, HStack, Modal } from '@chakra-ui/react';
 import { Client } from "@hiveio/dhive";
 import { FaGift } from 'react-icons/fa';
 import SendHiveModal from 'lib/pages/wallet/hive/sendHiveModal';
 
 interface Author {
-    name?: string;
-    posting_json_metadata?: string;
-    profile?: {
-      location?: string | null | undefined;
-      cover_image?: string | null | undefined;
-      about?: string | null | undefined;
-    } | undefined;
-    location?: string;
-    balance?: string;
-    hbd_balance?: string;
-  }
-  
-  
-  interface AuthorProfile {
-    cover_image?: string;
-    about?: string;
-    location?: string;
-    balance?: string;
-    hbd_balance?: string;
-  }
-  
-  
-  interface AuthorProfile {
-    cover_image?: string;
-    about?: string;
-    location?: string;
-    balance?: string;
-    hbd_balance?: string;
-  }
-  
-function calculateHumanReadableReputation(reputation:number) {
+  name?: string;
+  posting_json_metadata?: string;
+  profile?: {
+    location?: string | null | undefined;
+    cover_image?: string | null | undefined;
+    about?: string | null | undefined;
+  } | undefined;
+  location?: string;
+  balance?: string;
+  hbd_balance?: string;
+  last_account_update?: string;
+}
+
+interface AuthorProfile {
+  cover_image?: string;
+  about?: string;
+  location?: string;
+  balance?: string;
+  hbd_balance?: string;
+}
+
+function calculateHumanReadableReputation(reputation: number) {
   if (reputation === 0) {
     return 25;
   }
@@ -63,18 +54,17 @@ function SubscriberList() {
   const [isLoading, setIsLoading] = useState(true);
   const subscribersPerPage = 20;
   const [searchQuery, setSearchQuery] = useState<string>('');
-  const [userCount, setUserCount] = useState<number>(0); // Initialize userCount to 0
+  const [userCount, setUserCount] = useState<number>(0);
   const [isSendModalOpen, setIsSendModalOpen] = useState(false);
   const [toAddress, setToAddress] = useState("");
-const [amount, setAmount] = useState("");
-const [transferMemo, setTransferMemo] = useState("");
+  const [amount, setAmount] = useState("");
+  const [transferMemo, setTransferMemo] = useState("");
 
-  
   const fetchSubscribersWithPagination = async (lastSubscriberName?: string) => {
     try {
       const bridgeApiUrl = 'https://api.hive.blog';
       const communityName = 'hive-173115';
-  
+
       const bridgeRequestData = {
         jsonrpc: '2.0',
         method: 'bridge.list_subscribers',
@@ -85,9 +75,9 @@ const [transferMemo, setTransferMemo] = useState("");
         },
         id: 1,
       };
-  
+
       const response = await axios.post(bridgeApiUrl, bridgeRequestData);
-  
+
       if (response.data && response.data.result) {
         const subscriberUsernames = response.data.result.map((subscriber: any) => subscriber[0]);
 
@@ -103,7 +93,7 @@ const [transferMemo, setTransferMemo] = useState("");
               "techcoderx.com"
             ]);
             const account = await client.database.getAccounts([username]);
-  
+
             if (!account || !account.length) {
               return {
                 username,
@@ -113,10 +103,12 @@ const [transferMemo, setTransferMemo] = useState("");
                 cover_image: null,
                 location: null,
                 balance: null,
-                hbd_balance: null
+                hbd_balance: null,
+                last_account_update: null
               };
             }
-            // console.log(account[0].hbd_balance)
+
+            const last_account_update = account[0].last_account_update;
             const hbd_balance = account[0].hbd_balance;
             const balance = account[0].balance;
             const metadata = account[0].posting_json_metadata;
@@ -127,7 +119,7 @@ const [transferMemo, setTransferMemo] = useState("");
                 about: null,
               },
             };
-  
+
             try {
               if (metadata) {
                 parsedMetadata = JSON.parse(metadata) as any;
@@ -135,7 +127,7 @@ const [transferMemo, setTransferMemo] = useState("");
             } catch (error) {
               console.error(`Error parsing JSON metadata for ${username}:`, error);
             }
-  
+
             const location = parsedMetadata.profile?.location || null;
             const cover_image = parsedMetadata.profile?.cover_image || null;
             const authorAbout = parsedMetadata.profile?.about || null;
@@ -159,7 +151,8 @@ const [transferMemo, setTransferMemo] = useState("");
               cover_image,
               location,
               balance,
-              hbd_balance
+              hbd_balance,
+              last_account_update
             };
           } catch (error) {
             console.error(`Error fetching additional info for ${username}:`, error);
@@ -172,10 +165,11 @@ const [transferMemo, setTransferMemo] = useState("");
               location: null,
               balance: 0,
               hbd_balance: 0,
+              last_account_update: null
             };
           }
         });
-  
+
         const newSubscriberInfo = await Promise.all<any>(subscriberInfoPromises);
         setUserCount((prevUserCount) => prevUserCount + subscriberUsernames.length);
 
@@ -187,7 +181,6 @@ const [transferMemo, setTransferMemo] = useState("");
       setIsLoading(false);
     }
   };
-  
 
   useEffect(() => {
     fetchSubscribersWithPagination();
@@ -199,167 +192,167 @@ const [transferMemo, setTransferMemo] = useState("");
     fetchSubscribersWithPagination(lastSubscriberName);
     setIsLoading(true);
   };
-
+  function formatLastUpdateDate(lastUpdate: string | null) {
+    if (!lastUpdate) {
+      return "";
+    }
+    
+    const date = new Date(lastUpdate);
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const formattedDate = `${monthNames[date.getMonth()]} ${date.getFullYear()}`;
+    
+    return formattedDate;
+  }
+  
   const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const query = event.target.value.toLowerCase();
     setSearchQuery(query);
   };
 
-
   const filteredSubscribers = subscribers.filter((subscriber) =>
     subscriber.username.toLowerCase().includes(searchQuery)
   );
   const sortedSubscribers = [...filteredSubscribers].sort((a, b) => {
-    // You can customize the sorting logic here
-    // Here, we are sorting by whether they have voted or not
     if (a.hasVotedWitness && !b.hasVotedWitness) {
-      return -1; // a comes first
+      return -1;
     } else if (!a.hasVotedWitness && b.hasVotedWitness) {
-      return 1; // b comes first
+      return 1;
     } else {
-      return 0; // no change in order
+      return 0;
     }
   });
-  const handleSendModalOpen = (username: string) => { 
-    const defaultMemo = "🛹 Thank you for Voting on Skatehive Witness 🛹 "; 
-    setIsSendModalOpen(true);
-    setToAddress(username); // Set the toAddress to the username
-    setTransferMemo(defaultMemo); // Set the memo to the default value
 
+  const handleSendModalOpen = (username: string) => {
+    const defaultMemo = "🛹 Thank you for Voting on Skatehive Witness 🛹 ";
+    setIsSendModalOpen(true);
+    setToAddress(username);
+    setTransferMemo(defaultMemo);
   };
-  
 
   const handleSendModalClose = () => {
     setIsSendModalOpen(false);
   };
+
   return (
-<Flex flexDirection="column" alignItems="center" justifyContent="center">
-<Text>Users Loaded: {userCount}</Text> {/* Display user count */}
+    <Flex flexDirection="column" alignItems="center" justifyContent="center">
+      <Text>Users Loaded: {userCount}</Text>
+      <Input
+        type="text"
+        placeholder="Search by username"
+        value={searchQuery}
+        maxWidth={'50%'}
+        onChange={handleSearchInputChange}
+        marginBottom="10px"
+      />
+      {isLoading ? (
+        <Flex justifyContent="center" alignItems="center" flexDirection={"column"}>
+          <Image boxSize={"60px"} src='https://64.media.tumblr.com/12da5f52c1491f392676d1d6edb9b055/870d8bca33241f31-7b/s400x600/fda9322a446d8d833f53467be19fca3811830c26.gif'></Image>
+          <Button
+            border="1px solid limegreen"
+            color="white"
+            bg="black"
+            onClick={handleLoadMore}
+            marginTop="10px"
+            isDisabled={true}
+          >
+            Loading...
+          </Button>
+        </Flex>
+      ) : (
+        <Button
+          border="1px solid limegreen"
+          color="white"
+          bg="black"
+          onClick={handleLoadMore}
+          marginTop="10px"
+        >
+          Load More
+        </Button>
+      )}
 
-  <Input
-    type="text"
-    placeholder="Search by username"
-    value={searchQuery}
-    maxWidth={'50%'}
-    onChange={handleSearchInputChange}
-    marginBottom="10px"
-  />
-{isLoading ? (
-    <Flex justifyContent="center" alignItems="center" flexDirection={"column"}>
-<Image boxSize={"60px"} src='https://64.media.tumblr.com/12da5f52c1491f392676d1d6edb9b055/870d8bca33241f31-7b/s400x600/fda9322a446d8d833f53467be19fca3811830c26.gif'></Image>
-  <Button
-    border="1px solid limegreen"
-    color="white"
-    bg="black"
-    onClick={handleLoadMore}
-    marginTop="10px"
-    isDisabled={true} // Disable the button when loading
-  >
-    Loading...
-  </Button>
-  </Flex>
-) : (
-  <Button
-    border="1px solid limegreen"
-    color="white"
-    bg="black"
-    onClick={handleLoadMore}
-    marginTop="10px"
-  >
-    Load More
-  </Button>
-)}
+      <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
+        {sortedSubscribers.map((subscriberInfo) => (
+          <Box
+            key={subscriberInfo.username}
+            p={4}
+            borderWidth="1px"
+            borderRadius="lg"
+            boxShadow="md"
+            m={2}
+            width="380px"
+            height="512px"
+            transition="transform 0.2s"
+            _hover={{ transform: 'scale(1.05)' }}
+          >
+            <HStack justifyContent={'center'}>
+              <Avatar
+                src={`https://images.ecency.com/webp/u/${encodeURIComponent(subscriberInfo.username)}/avatar/small`}
+                size="lg"
+                boxSize={20}
+                borderRadius="50%"
+                border="2px solid limegreen"
+                marginBottom="10px"
+              />
+              <Link href={`https://skatehive.app/profile/${subscriberInfo.username}`} target="_blank" rel="noopener noreferrer">
+                <Text color={'orange'} fontSize="lg" fontWeight="bold" textAlign="center">
+                  {subscriberInfo.username}
+                </Text>
+              </Link>
+              <Button variant={"ghost"} onClick={() => handleSendModalOpen(subscriberInfo.username)}>
+                <FaGift size={30} color="orange" />
+              </Button>
+            </HStack>
 
+            <Flex align="center" flexDirection="column" justifyContent="center">
+              <Divider margin="10px" />
+              <Table variant="simple">
+                <Tbody>
+                  <Tr>
+                    <Td>Witness</Td>
+                    <Td>{subscriberInfo.hasVotedWitness ? '✅ Voted' : '❌ Not Yet'}</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Reputation</Td>
+                    <Td>{calculateHumanReadableReputation(subscriberInfo.reputation)}</Td>
+                  </Tr>
+                  <Tr>
+  <Td>Location</Td>
+  <Td style={{ whiteSpace: 'break-spaces' }}>{subscriberInfo.location}</Td>
+</Tr>
 
-    <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
-    {sortedSubscribers.map((subscriberInfo) => (
-        <Box
-  key={subscriberInfo.username}
-  p={4}
-  borderWidth="1px"
-  borderRadius="lg"
-  boxShadow="md"
-  m={2}
-  width="420px"
-  height="500px"
-  transition="transform 0.2s"
-  _hover={{ transform: 'scale(1.05)' }}
->
-    <HStack justifyContent={'center'}>
-    <Avatar
-      src={`https://images.ecency.com/webp/u/${encodeURIComponent(subscriberInfo.username)}/avatar/small`}
-      size="lg"
-      boxSize={20}
-      borderRadius="50%"
-      border="2px solid limegreen"
-      marginBottom="10px"
-    />
-    <Link href={`https://skatehive.app/profile/${subscriberInfo.username}`} target="_blank" rel="noopener noreferrer">
-
-    <Text color={'orange'} fontSize="lg" fontWeight="bold" textAlign="center">
-      {subscriberInfo.username}
-    </Text>
-    </Link>
-
-    <Button variant={"ghost"} onClick={() => handleSendModalOpen(subscriberInfo.username)}>
-  <FaGift size={30} color="orange" />
-</Button>
-
-
-</HStack>
-
-  <Flex align="center" flexDirection="column" justifyContent="center">
-
-    <Divider margin="10px" />
-    
-    <Table variant="simple">
-
-      <Tbody>
-        <Tr>
-          <Td>Witness</Td>
-          <Td>{subscriberInfo.hasVotedWitness ? '✅ Voted' : '❌ Not Yet'}</Td>
-        </Tr>
-        <Tr>
-          <Td>Reputation</Td>
-          <Td>{calculateHumanReadableReputation(subscriberInfo.reputation)}</Td>
-        </Tr>
-        <Tr>
-          <Td>Location</Td>
-          <Td >{subscriberInfo.location}</Td>
-        </Tr>
-        <Tr>
-          <Td>Balance</Td>
-          <Td>{subscriberInfo.balance}</Td>
-        </Tr>
-        <Tr>
-          <Td>HBD Balance</Td>
-          <Td>{subscriberInfo.hbd_balance}</Td>
-        </Tr>
-      </Tbody>
-    </Table>
-  </Flex>
-  {isSendModalOpen && (
-  <Modal isOpen={isSendModalOpen} onClose={handleSendModalClose}>
-  <SendHiveModal
-    showModal={isSendModalOpen}
-    setShowModal={setIsSendModalOpen}
-    toAddress={toAddress}
-    setToAddress={setToAddress}
-    amount={amount}
-    setAmount={setAmount}
-    hiveMemo={transferMemo} 
-    setHiveMemo={setTransferMemo} 
-  />
-</Modal>
-)}
-
-
-</Box>
-      ))}
+                  <Tr>
+                    <Td>Balance</Td>
+                    <Td>{subscriberInfo.balance}</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>HBD Balance</Td>
+                    <Td>{subscriberInfo.hbd_balance}</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Last Update</Td>
+                    <Td>{formatLastUpdateDate(subscriberInfo.last_account_update)}</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </Flex>
+            {isSendModalOpen && (
+              <Modal isOpen={isSendModalOpen} onClose={handleSendModalClose}>
+                <SendHiveModal
+                  showModal={isSendModalOpen}
+                  setShowModal={setIsSendModalOpen}
+                  toAddress={toAddress}
+                  setToAddress={setToAddress}
+                  amount={amount}
+                  setAmount={setAmount}
+                  hiveMemo={transferMemo}
+                  setHiveMemo={setTransferMemo}
+                />
+              </Modal>
+            )}
+          </Box>
+        ))}
+      </Flex>
     </Flex>
-
-</Flex>
-
   );
 }
 

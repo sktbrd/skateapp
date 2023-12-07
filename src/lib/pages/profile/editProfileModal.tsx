8888ebfb3f96e116789cbb4af0353f725def1fff
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-12-07 01:55:27 -0300

    Profile Images Upload with IPFS

--- src/lib/pages/profile/editProfileModal.tsx
+++ src/lib/pages/profile/editProfileModal.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Textarea, Button, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text } from '@chakra-ui/react';
 import { KeychainSDK, KeychainKeyTypes, Broadcast } from 'keychain-sdk';
-import { css } from '@emotion/react'; // Add this line
+import { css } from '@emotion/react';
+import axios from 'axios';
 
 interface EditProfileModalProps {
   isOpen: boolean;
@@ -9,18 +10,23 @@ interface EditProfileModalProps {
   user: any;
 }
 
+const PINATA_API_KEY = process.env.PINATA_API_KEY;
+const PINATA_API_SECRET = process.env.PINATA_API_SECRET;
+const PINATA_GATEWAY_TOKEN = process.env.PINATA_GATEWAY_TOKEN;
+
 const EditProfileModal: React.FC<EditProfileModalProps> = ({ isOpen, onClose, user }) => {
-    const [name, setName] = useState<string>(user.profile?.name || '');
-    const [about, setAbout] = useState<string>(user.profile?.about || '');
-    const [avatarUrl, setAvatarUrl] = useState<string>(user.profile?.profile_image || '');
-    const [coverImageUrl, setCoverImageUrl] = useState<string>(user.profile?.cover_image || '');
-    const [website, setWebsite] = useState<string>(user.profile?.website || '');
+  const [name, setName] = useState<string>(user.profile?.name || '');
+  const [about, setAbout] = useState<string>(user.profile?.about || '');
+  const [avatarUrl, setAvatarUrl] = useState<string>(user.profile?.profile_image || '');
+  const [coverImageUrl, setCoverImageUrl] = useState<string>(user.profile?.cover_image || '');
+  const [website, setWebsite] = useState<string>(user.profile?.website || '');
+  const [selectedProfileFile, setSelectedProfileFile] = useState<File | null>(null);
+  const [selectedCoverFile, setSelectedCoverFile] = useState<File | null>(null);
 
   useEffect(() => {
     if (user && user.posting_json_metadata) {
       try {
         const metadata = JSON.parse(user.posting_json_metadata);
-        // Update the state values only if they are not already set
         setName(name => name || metadata.profile.name || '');
         setAbout(about => about || metadata.profile.about || '');
         setAvatarUrl(avatarUrl => avatarUrl || metadata.profile.profile_image || '');
@@ -63,12 +69,64 @@ const EditProfileModal: React.FC<EditProfileModalProps> = ({ isOpen, onClose, us
         },
       };
 
-      // Convert 'formParamsAsObject' to 'unknown' first
       const broadcast = await keychain.broadcast(formParamsAsObject.data as unknown as Broadcast);
-
 
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const uploadFileToIPFS = async (file: File) => {
+    try {
+      if (file.type.startsWith("video/mp4")) {
+        alert("Video file upload is not supported yet.");
+      } else if (file.type.startsWith("image/")) {
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.set("Content-Type", "multipart/form-data");
+
+        const response = await fetch(
+          "https://api.pinata.cloud/pinning/pinFileToIPFS",
+          {
+            method: "POST",
+            headers: {
+              "pinata_api_key": PINATA_API_KEY,
+              "pinata_secret_api_key": PINATA_API_SECRET,
+            },
+            body: formData,
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          const ipfsUrl = `https://ipfs.skatehive.app/ipfs/${data.IpfsHash}?pinataGatewayToken=nxHSFa1jQsiF7IHeXWH-gXCY3LDLlZ7Run3aZXZc8DRCfQz4J4a94z9DmVftXyFE`;
+          console.log(ipfsUrl);
+          return ipfsUrl;
+        } else {
+          const errorData = await response.json();
+          console.error("Error uploading image file to Pinata IPFS:", errorData);
+        }
+      } else {
+        alert("Invalid file type. Only images are allowed.");
+      }
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
+  };
+
+  const handleProfileFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setSelectedProfileFile(file);
+      uploadFileToIPFS(file).then((ipfsUrl) => setAvatarUrl(ipfsUrl?.toString() || ''));
+    }
+  };
+
+  const handleCoverFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setSelectedCoverFile(file);
+      uploadFileToIPFS(file).then((ipfsUrl) => setCoverImageUrl(ipfsUrl?.toString() || ''));
     }
   };
 
@@ -85,17 +143,16 @@ const EditProfileModal: React.FC<EditProfileModalProps> = ({ isOpen, onClose, us
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-            <Text> About</Text>
-            <Textarea
-                placeholder="About"
-                value={about}
-                onChange={(e) => setAbout(e.target.value)}
-                css={css`
-                    white-space: wrap;
-                    min-height: 150px; // Adjust the minHeight value as needed
-                `}
-                />
-
+          <Text> About</Text>
+          <Textarea
+            placeholder="About"
+            value={about}
+            onChange={(e) => setAbout(e.target.value)}
+            css={css`
+              white-space: wrap;
+              min-height: 150px;
+            `}
+          />
           <Text> Avatar URL</Text>
           <Input
             placeholder="Avatar URL"
@@ -108,12 +165,42 @@ const EditProfileModal: React.FC<EditProfileModalProps> = ({ isOpen, onClose, us
             value={coverImageUrl}
             onChange={(e) => setCoverImageUrl(e.target.value)}
           />
-            <Text> Website</Text>
-            <Input
-                placeholder="Website"
-                value={website}
-                onChange={(e) => setWebsite(e.target.value)}
+          <Text> Website</Text>
+          <Input
+            placeholder="Website"
+            value={website}
+            onChange={(e) => setWebsite(e.target.value)}
+          />
+          <Button color={"limegreen"} border={"1px dashed limegreen"} variant="ghost">
+            <label htmlFor="profileFileInput">Choose Profile Image</label>
+            <input
+              type="file"
+              id="profileFileInput"
+              accept="image/*"
+              style={{ display: 'none' }}
+              onChange={handleProfileFileInputChange}
             />
+          </Button>
+          {selectedProfileFile && (
+            <div>
+              Selected Profile Image: {selectedProfileFile.name}
+            </div>
+          )}
+          <Button color={"limegreen"} border={"1px dashed limegreen"} variant="ghost">
+            <label htmlFor="coverFileInput">Choose Cover Image</label>
+            <input
+              type="file"
+              id="coverFileInput"
+              accept="image/*"
+              style={{ display: 'none' }}
+              onChange={handleCoverFileInputChange}
+            />
+          </Button>
+          {selectedCoverFile && (
+            <div>
+              Selected Cover Image: {selectedCoverFile.name}
+            </div>
+          )}
         </ModalBody>
         <ModalFooter>
           <Button colorScheme="red" mr={3} onClick={onClose}>

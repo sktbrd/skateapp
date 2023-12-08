9e170cde507bf2a37c00e083e70c2ca6b22fa501
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-12-08 13:07:04 -0300

    debug knowhow profile2

--- src/lib/pages/profile/index.tsx
+++ src/lib/pages/profile/index.tsx
@@ -42,7 +42,8 @@ export default function ProfilePage() {
         try {
           const metadata = JSON.parse(user.posting_json_metadata || '');
           const coverImage = metadata.profile.cover_image;
-          console.log(metadata)
+          console.log("8=======D", metadata)
+          console.log("8=======D",user)
 
           if (coverImage) {
             setCoverImageUrl(coverImage);

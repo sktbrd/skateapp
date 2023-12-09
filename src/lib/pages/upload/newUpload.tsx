4a1218bd2d7b47bf201be223c72c366f9f40e69f
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-12-09 19:29:58 -0300

    new video upload undo

--- src/lib/pages/upload/newUpload.tsx
+++ src/lib/pages/upload/newUpload.tsx
@@ -160,7 +160,7 @@ const NewUpload: React.FC = () => {
   
           setUploadedVideo(ipfsUrl);
   
-          const transformedLink = `<video src="${ipfsUrl}" controls></video>` + " ";
+          const transformedLink = `<iframe src="${ipfsUrl}" allowfullscreen></iframe>` + " ";
   
           setMarkdownText((prevMarkdown) => prevMarkdown + `\n${transformedLink}` + '\n');
           setUploadedFiles((prevFiles) => [...prevFiles, ipfsUrl]);

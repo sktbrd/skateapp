bafdb490c13226eb15d910be56242d38bb3b2720
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-12-13 10:09:27 -0300

    handle empty posting_json_metadata, thanks r4to

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-require('dotenv').config(); 
-
+import dotenv from 'dotenv';
+dotenv.config();
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig, loadEnv } from "vite";

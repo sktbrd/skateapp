7f9470bb14f19c67800fa8f3957dfa2d8a4f9b5e
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-12-24 15:10:47 -0300

    Send Hive Tip in Comment Box

--- src/lib/pages/home/Feed/postModal/commentBox.tsx
+++ src/lib/pages/home/Feed/postModal/commentBox.tsx
@@ -2,16 +2,27 @@ import React, { useState } from 'react';
 import {
   Box,
   Textarea,
-  Button
+  Button,
+  Flex,
+  HStack,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  Input,
+  Text,
+  Image
 } from '@chakra-ui/react';
 
-import * as Types from '../types'
+import * as Types from '../types';
 import HiveLogin from '../../api/HiveLoginModal';
-
+import { FaEthereum, FaHive } from 'react-icons/fa';
+import sendHive from 'lib/pages/utils/hiveFunctions/sendHive';
 
 const CommentBox: React.FC<Types.CommentBoxProps> = ({ user, parentAuthor, parentPermlink, onCommentPosted }) => {
   const [commentContent, setCommentContent] = useState('');
-  const [isHiveLoginModalOpen, setHiveLoginModalOpen] = useState(false); // State to control HiveLoginModal
+  const [isHiveLoginModalOpen, setHiveLoginModalOpen] = useState(false);
+  const [isSendTipModalOpen, setSendTipModalOpen] = useState(false);
 
   const handleCommentSubmit = () => {
     if (!window.hive_keychain) {
@@ -25,7 +36,7 @@ const CommentBox: React.FC<Types.CommentBoxProps> = ({ user, parentAuthor, paren
       return;
     }
 
-    const permlink = new Date().toISOString().replace(/[^a-zA-Z0-9]/g, '').toLowerCase(); // Generate a unique permlink for the comment
+    const permlink = new Date().toISOString().replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
     const operations = [
       ["comment",
@@ -43,15 +54,85 @@ const CommentBox: React.FC<Types.CommentBoxProps> = ({ user, parentAuthor, paren
 
     window.hive_keychain.requestBroadcast(username, operations, "posting", (response: any) => {
       if (response.success) {
-        setCommentContent(''); // Clear the comment box
-
-        // Call the callback to force re-render of Comments component
+        setCommentContent('');
         onCommentPosted();
       } else {
         console.error("Error posting comment:", response.message);
       }
     });
   };
+
+  const sendHiveTipModal = () => {
+    return (
+      <Modal isOpen={isSendTipModalOpen} onClose={() => setSendTipModalOpen(false)}>
+        <ModalContent minW={"75%"} bg={"black"} border={"1px dashed limegreen"}>
+          <ModalCloseButton color={"red"} />
+          <ModalHeader>Send Hive Tip to {parentAuthor}</ModalHeader>
+          <center>
+
+            <Image boxSize={"148px"} src="assets/santapepe.png"></Image>
+          </center>
+          <HStack p="10px" justifyContent={"space-between"} m={"20px"}>
+            <Button
+              leftIcon={<FaHive color='red' />}
+              border="3px solid red"
+              mt="10px"
+              onClick={() => sendHive("0.1", parentAuthor || '', "Hive Tip")}
+              bg={"transparent"}
+              color={"white"}
+            > 0.1 HIVE</Button>
+            <Button
+              leftIcon={<FaHive color='red' />}
+              border="3px solid red"
+              mt="10px"
+              onClick={() => sendHive("1", parentAuthor || '', "Hive Tip")}
+              bg={"transparent"}
+              color={"white"}
+            > 1 HIVE</Button>
+            <Button
+              leftIcon={<FaHive color='red' />}
+              border="3px solid red"
+              mt="10px"
+              onClick={() => sendHive("10", parentAuthor || '', "Hive Tip")}
+              bg={"transparent"}
+              color={"white"}
+            > 10 HIVE</Button>
+            <Button
+              leftIcon={<FaHive color='red' />}
+              border="3px solid red"
+              mt="10px"
+              onClick={() => sendHive("50", parentAuthor || '', "Hive Tip")}
+              bg={"transparent"}
+              color={"white"}
+            > 50 HIVE</Button>
+          </HStack>
+          <Box p="10px" textAlign="center">
+            <Text textAlign="center" color={"white"}>Or enter a custom amount:</Text>
+
+            <Input margin={"2px"} placeholder="Custom Amount"
+              onChange={(e) => setCommentContent(e.target.value)}
+              maxW={"20%"}
+            />
+            <Button
+              leftIcon={<FaHive />}
+              border="1px solid white"
+              marginLeft={"10px"}
+              onClick={() => sendHive(commentContent, parentAuthor || '', "Hive Tip")}
+            >Tip {commentContent} HIVE</Button>
+          </Box>
+
+        </ModalContent>
+      </Modal>
+    );
+  }
+
+  const handleHiveTipClick = () => {
+    setSendTipModalOpen(true);
+  }
+  const handleEthereumTipClick = () => {
+    console.log("Ethereum tip clicked");
+    alert("comming soon, come on its xmas go mint the nft on the homepage")
+  }
 
   return (
     <Box margin={"10px"} borderRadius={"10px"} border="1px solid white" padding="10px" mt="20px">
@@ -60,9 +141,28 @@ const CommentBox: React.FC<Types.CommentBoxProps> = ({ user, parentAuthor, paren
         onChange={(e) => setCommentContent(e.target.value)}
         placeholder="Write your comment here..."
       />
-      <Button border="1px solid white" mt="10px" onClick={handleCommentSubmit}>
-        Submit Comment
-      </Button>
+      <HStack justifyContent={"space-between"}>
+        <Flex>
+          <Button
+            leftIcon={<FaHive color='red' />}
+            border="1px solid white"
+            mt="10px"
+            onClick={handleHiveTipClick}
+          >Tip</Button>
+          <Button
+            leftIcon={<FaEthereum color='blue' />}
+            border="1px solid white"
+            mt="10px"
+            marginLeft="10px"
+            onClick={handleEthereumTipClick}
+          >Tip</Button>
+        </Flex>
+        <Button border="1px solid white" mt="10px" onClick={handleCommentSubmit}>
+          Submit Comment
+        </Button>
+      </HStack>
+
+      {sendHiveTipModal()}
       {isHiveLoginModalOpen && (
         <HiveLogin isOpen={true} onClose={() => setHiveLoginModalOpen(false)} />
       )}

8c73836e48b60eeb405125ca58546c7b5380e245
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-01-10 16:24:41 -0300

    fix: plaza load comments

---
+++ src/lib/pages/utils/hiveClient.tsx
@@ -0,0 +1,14 @@
+import { Client } from "@hiveio/dhive";
+
+const HiveClient = new Client([
+  // Not working
+  // "https://api.hive.blog/",
+  // "https://api.hivekings.com",
+  "https://anyx.io",
+  "https://api.openhive.network",
+  "https://hived.privex.io",
+  "https://rpc.ausbit.dev",
+  "https://techcoderx.com",
+]);
+
+export default HiveClient;

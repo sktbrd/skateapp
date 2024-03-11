4655d3f49d3aeed8d9e1236c42251f82876d8975
¨sktbrd¨ <sktbrd.eth@gmail.com>
2024-03-11 14:50:30 -0600

    swappage

--- src/lib/pages/swap/index.tsx
+++ src/lib/pages/swap/index.tsx
@@ -1,8 +1,12 @@
+import { Container } from "@chakra-ui/react";
+
+
+
 const SwapComponent = () => {
     return (
-        <div>
-            <iframe id="simpleswap-frame" name="SimpleSwap Widget" width="528px" height="392px" src="https://simpleswap.io/widget/df29d743-6c03-4c7e-a745-4a0bfd19c656" ></iframe>    );
-        </div>
+        <Container marginTop={"8%"}>
+            <iframe id="simpleswap-frame" name="SimpleSwap Widget" width="528px" height="392px" src="https://simpleswap.io/widget/df29d743-6c03-4c7e-a745-4a0bfd19c656" ></iframe>
+        </Container>
     );
 }
 export default SwapComponent;

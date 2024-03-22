ca855a74a04787332b0995bc9fbbc27f3ea17bc3
¨sktbrd¨ <sktbrd.eth@gmail.com>
2024-03-22 00:25:20 -0300

    Style: loading bar

--- src/lib/pages/home/Feed/Feed.tsx
+++ src/lib/pages/home/Feed/Feed.tsx
@@ -53,27 +53,7 @@ const defaultThumbnail =
 const placeholderEarnings = 69.42;
 
 const randomSentences = [
-  "Don't mall grab, or do it, you do you...",
-  "'Ok to push Mongo, it is! -master yoda'",
-  "Roll one, and play some stoken.quest?",
-  "Remember Mirc times?",
-  "Fuck instagram!",
-  "Ready to grind on chain?",
-  "Praise whoever made skatevideosite",
-  "Loading Stokenomics...",
-  "Initiating Proof of Stoke...",
-  "We will load as fast as Daryl Rolls",
-  "Who was Gnartoshi Shredmoto?",
-  "We have secret sections here, can you find?",
-  "We dont store any data, we dont even know how to do that",
-  "P-rod said that NOW the flip ins and flip outs are too much...",
-  "SkateHive is really made by skaters, that actually skate, the 4 stances and more.",
-  "If its a quick post, do it in Plaza. If its a long form post do it in Mag",
-  "If you need help, roll a joint and joint or grab a beer and discord.gg/skateboard",
-  "You can downvote stuff if you think it sucks",
-  "One day you will get our /wallet and /dao pages, take your time...",
-  "Do you remember your first kickflip? I dont I am a robot",
-  "You Skate? Congrats this site is yours and its money is for you.",
+  "Bem Vindo a Bless ",
 
 ];
 
@@ -84,12 +64,12 @@ const PlaceholderLoadingBar = () => {
 
   return (
     <center>
-      <Text marginBottom={"12px"} >{randomSentence}</Text>
+      <Text color='white' marginBottom={"12px"} >{randomSentence}</Text>
 
       <Image
         borderRadius={"20px"}
         boxSize="100%"
-        src="/assets/pepenation.gif"
+        src="https://cdn.dribbble.com/users/921277/screenshots/13742829/media/585c0fa10c1caa6b87a6d32cab3ccd9c.gif"
       />
     </center>
   );
